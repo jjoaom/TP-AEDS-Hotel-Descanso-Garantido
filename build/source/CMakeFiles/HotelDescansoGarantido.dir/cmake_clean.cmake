ab55042f16458d08a08f00c7a47001866b6a413a
jjoaom <joaomarcos.aquino@hotmail.com>
2024-07-04 17:30:43 -0300

    updated cmake config

---
+++ build/source/CMakeFiles/HotelDescansoGarantido.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HotelDescansoGarantido.dir/main.cpp.o"
+  "CMakeFiles/HotelDescansoGarantido.dir/main.cpp.o.d"
+  "HotelDescansoGarantido"
+  "HotelDescansoGarantido.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HotelDescansoGarantido.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ source/build/CMakeFiles/HotelDescansoGarantido.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for HotelDescansoGarantido.
+# This may be replaced when dependencies are built.
